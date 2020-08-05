e4801e8cd756810d053000ea324f14c547889515
anivedh555 <anivedh.21810720@viit.ac.in>
2020-08-05 23:29:17 +0530

    Linked lists trial without driver program

---
+++ DS/list.h
@@ -0,0 +1,38 @@
+/*********************list.h*************************************/
+
+
+#ifndef LIST_H
+#define LIST_H
+
+#include<stdlib.h>
+
+
+//-------------Structure for List element----------------
+typedef struct ListElmt_ {
+	void *data;
+	struct ListElmt_ *next;
+}ListElmt;
+
+//------------Structure for linkedlist------------------
+
+typedef struct List_ {
+	int size;
+	int (*match)(const void *key1, const void *key2);
+	void (*destroy)(void *data)
+	ListElmt *head;
+	ListElmt *tail;
+}List;
+
+void list_init(List *list, void (*destroy)(void *data));
+void list_destory(List *list);
+int list_ins_next(List *list, ListElmt *element, const void *data);
+int list_rem_next(List *list, ListElmt *element, void **data);
+
+#define list_size(list) ((list)->size)
+#define list_head(list) ((list)->head)
+#define list_tail(list) ((list)->tail)
+#define list_is_head(list, element) ((element)==(list)->head ? 1:0)
+#define list_is_tail(list, element) ((element)->next==NULL 1:0)
+#define list_data(element) ((element)->data)
+#define list_next(element) ((element)->next)
+#endif
